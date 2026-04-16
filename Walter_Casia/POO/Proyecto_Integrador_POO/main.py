3041e945619e47d46c10fcd2c9ad9f6282ef79cb
Walter Casia <3031530400108@ingenieria.usac.edu.gt>
2026-04-16 08:41:15 -0600

    act

--- Walter_Casia/POO/Proyecto_Integrador_POO/main.py
+++ Walter_Casia/POO/Proyecto_Integrador_POO/main.py
@@ -1,20 +1,30 @@
+import random
+
 class Pokemon:
     def __init__(self, nombre, hp_actual, hp_maximo, energia_actual, energia_maxima):
         self.nombre = nombre
+        self.__hp_actual = hp_actual
         self.__hp_maximo = hp_maximo
         self.__energia_actual = energia_actual
         self.__energia_maxima = energia_maxima
 
     @property
     def hp_maximo(self):
-        return self.hp_maximo
+        return self.__hp_maximo
     
     @property
-    def hnergia_actual(self):
-        return self.nergia_actual
+    def hp_actual(self):
+        return self.__hp_actual
+    
+    @property
+    def energia_actual(self):
+        return self.__energia_actual
     
     @property
     def energia_maxima(self):
-        return self.energia_maxima
+        return self.__energia_maxima
     
+    @hp_maximo.setter
+    def hp_maximo(self, valor):
+        hp_maximo = valor
 
