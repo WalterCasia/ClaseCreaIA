5a74682ba751aeaf1948d5161b7e5ddec4da8ab8
Walter Casia <3031530400108@ingenieria.usac.edu.gt>
2026-05-18 11:56:23 -0600

    act

---
+++ Walter_Casia/Programacion_Avanzada/S2/D1/proyecto-js-html-css-express/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const PORT = 4000;
+
+app.use(express.static(path.join(__dirname, ' public')));
+
+app.use(express.urlencoded({extended: true}))
+
+app.post('/enviar_datos', (res, req) =>{
+    const{nombre, id} = req.body;
+    console.log(`Nombre: ${nombre} ID: ${id} `)
+    res.setEncoding('Datos recibidos correctamente')
+});
+
+app.listen(PORT, () =>{
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+})
